b3b9d72837c4f169149e1b079785f234ddb7d521
TaamZID <47922615+TaamZID@users.noreply.github.com>
2021-08-02 01:34:13 +0600

    Delete Controller directory

--- Controller/deleteTree.php
+++
@@ -1,8 +0,0 @@
-<?php
-require '../Model/db_connect.php';
-$id = $_GET['id'];
-$sql = 'DELETE FROM tree WHERE id=:id';
-$statement = $conn->prepare($sql);
-if ($statement->execute([':id' => $id])) {
-  header("Location:../View/showAllTrees.php");
-}
--- Controller/showTrees.php
+++
@@ -1,7 +0,0 @@
-<?php
-require '../Model/db_connect.php';
-$sql = 'SELECT * FROM tree';
-$statement = $conn->prepare($sql);
-$statement->execute();
-$tree = $statement->fetchAll(PDO::FETCH_OBJ);
-?>
