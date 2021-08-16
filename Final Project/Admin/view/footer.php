909adf84fc2844cf8dcfdf4478c65836073dfc7f
TaamZID <47922615+TaamZID@users.noreply.github.com>
2021-08-16 17:10:22 +0600

    Delete Final Project directory

--- Final Project/Admin/view/footer.php
+++
@@ -1,6 +0,0 @@
-<hr style="width:25%;text-align:center;margin-left:560">
-<p style='text-align: center;'>Copyrights © 2021 Tamzid. All rights reserved.</p>
-
-
-</body>
-</html>
--- Final Project/Planter/controller/delete_tree_controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once '../model/model.php';
-
-if (delete_tree($_POST['id'])) {
-    header('Location: ../view/show_all_tree.php');
-}
-?>
